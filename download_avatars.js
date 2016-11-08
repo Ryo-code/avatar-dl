f69e54c5b381acef4d5b99ea8e67515c92983afe
Ryo <kanadajinlee@yahoo.ca>
2016-11-08 18:14:29 +0000

    concatenated the requestURL

--- download_avatars.js
+++ download_avatars.js
@@ -3,11 +3,16 @@
 const request = require('request');
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
-  
-}
 
+function getRepoContributors(repoOwner, repoName, cb) {
+
+  const GITHUB_USER = "Ryo-code";
+  const GITHUB_TOKEN = "place-holder-token"; //don't push the token to github
+
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+
+  console.log('requestURL------>', requestURL);
+}
 
 getRepoContributors("mhdawson", "CurryKitten", function(err, result) {
   console.log("Errors:", err);
