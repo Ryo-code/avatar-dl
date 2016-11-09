07cdb8f2ef70fa3c57540b95a98108051e9d5aaf
Ryo <kanadajinlee@yahoo.ca>
2016-11-09 02:43:06 +0000

    Added my access token and git-ignored it so I can't have my identity stolen or whatever

--- download_avatars.js
+++ download_avatars.js
@@ -1,16 +1,24 @@
 'use strict';
 const request = require('request');
 console.log('Welcome to the GitHub Avatar Downloader!');
-
+const GITHUB_USER = "Ryo-code";
+const githubToken = require('./gh-access-token');
+console.log("Token: ", githubToken);
 
 function getRepoContributors(repoOwner, repoName, cb) {
 
-  const GITHUB_USER = "Ryo-code";
-  const GITHUB_TOKEN = "place-holder-token"; //秘密だ！Don't push your token to github!
-
-  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  const requestURL = 'https://' + GITHUB_USER + ':' + githubToken.token + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
 
   console.log('requestURL------>', requestURL);
+
+  request.get(requestURL)
+         .on('error', function (err) {      // Note 2
+           throw err;
+         })
+         .on('response', function (response) {  // Note 3
+          console.log('Response Status Code: ', response.statusCode);
+
+         })
 }
 
 getRepoContributors("mhdawson", "CurryKitten", function(err, result) {
