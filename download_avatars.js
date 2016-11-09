526f088646e7879a277730ba58edab14c87edff1
Ryo <kanadajinlee@yahoo.ca>
2016-11-09 17:50:22 +0000

    parsed the JSON string into an object, and passed that to the cb function

--- download_avatars.js
+++ download_avatars.js
@@ -1,14 +1,17 @@
 'use strict';
 const request = require('request');
-console.log('Welcome to the GitHub Avatar Downloader!');
 const GITHUB_USER = "Ryo-code";
 const githubToken = require('./gh-access-token');
-console.log("Token: ", githubToken);
+
+const owner = process.argv[2];
+const repo = process.argv[3];
+
+console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
 
-  // const requestURL = `https://${GITHUB_USER}:${githubToken.token}@api.github.com/repos/${repoOwner}/${repoName}/contributors`;
-  const requestURL = 'https://' + GITHUB_USER + ':' + githubToken.token + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  const endpoint = `api.github.com/repos/${repoOwner}/${repoName}/contributors`;
+  const requestURL = `https://${GITHUB_USER}:${githubToken.token}@${endpoint}`;
 
   const options = {
     url: requestURL,
@@ -19,18 +22,27 @@ function getRepoContributors(repoOwner, repoName, cb) {
 
   console.log('requestURL------>', requestURL);
 
-  request.get(options)
-         .on('error', function (err) {      // Note 2
-           throw err;
-         })
-         .on('response', function (response) {  // Note 3
-          // const parsedResponse = JSON.parse(response[0]);
-          // console.log("Parsed response: ", response);
-          console.log('Response Status Code: ', response.statusCode);
-         })
+  request
+    .get(options)
+    .on('error', function(err) {
+      cb(err, null);
+    })
+    .on('response', function(response) {
+      let _data = '';
+
+      response.on('data', function(data) {
+        _data += data;
+      })
+
+      response.on('end', function() {
+        const parsedResponse = JSON.parse(_data);
+        cb(null, parsedResponse)
+      });
+    });
 }
 
-getRepoContributors("mhdawson", "CurryKitten", function(err, result) {
+getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
+
 });
